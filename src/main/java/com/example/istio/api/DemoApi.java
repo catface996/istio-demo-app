e6b9dc1678626cfa26f695bfc14a75b3393f0c1e
大猫 <zlyxzq@163.com>
2022-05-02 11:53:35 +0800

    [新增功能](main): 修改注释。

--- src/main/java/com/example/istio/api/DemoApi.java
+++ src/main/java/com/example/istio/api/DemoApi.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author by 大猫
  * @date 2022/2/22 8:38 PM
  * <p>
- * Copyright 2021  北京交个朋友数码科技有限公司, Inc. All rights reserved.
+ * Copyright 2021  catface996, Inc. All rights reserved.
  */
 @FeignClient(url = "${service.url}", name = "demoApi", fallbackFactory = DemoApiFallback.class)
 public interface DemoApi {
--- src/main/java/com/example/istio/api/DemoApiFallback.java
+++ src/main/java/com/example/istio/api/DemoApiFallback.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
  * @author by 大猫
  * @date 2022/2/22 8:44 PM
  * <p>
- * Copyright 2021  北京交个朋友数码科技有限公司, Inc. All rights reserved.
+ * Copyright 2021  catface996, Inc. All rights reserved.
  */
 @Component
 public class DemoApiFallback implements FallbackFactory<DemoApi> {
